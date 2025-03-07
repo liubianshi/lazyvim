8de7139c94d2e6380d64f55d662ef8c4fbd87339
liubianshi <liu.bian.shi@gmail.com>
2025-03-07 11:22:18 +0800

    feat: add everforest colorscheme and configure lazyvim to use it
    This commit introduces the 'everforest' colorscheme to the
    Neovim configuration. It also configures LazyVim to use
    'everforest' as the default colorscheme. This enhances the
    user's visual experience by providing a visually appealing
    and well-designed color palette for the editor.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "vague2k/vague.nvim",
+    opts = {
+      transparent = false,
+    },
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      ---@diagnostic disable: missing-fields
+      require("everforest").setup({
+        background = "hard",
+        transparent_background_level = 2,
+        italics = true,
+        disable_italic_comments = true,
+      })
+    end,
+  },
+}
