240d310d6a62b861b7186acf1ecd7aa5567caa05
liu.bian.shi <liu.bian.shi@gmail.com>
2025-03-31 09:34:22 +0800

    refactor(r.lua): rewrite R snippets using table return format
    This commit refactors the R snippets to use the table return
    format, which is the recommended way to define snippets in
    LuaSnip. This change improves the structure and readability
    of the snippet definitions.
    
    feat(r.lua): add exported function stub snippet with Roxygen comments
    
    This commit introduces a new snippet for creating exported
    functions with Roxygen comments. The snippet is triggered by
    `@` followed by a comma-separated list of function name and
    parameters. It automatically generates the Roxygen comment
    block with `@param` tags for each parameter and `@return` tag.
    This addition streamlines the process of documenting R functions
    and ensures consistency in documentation style.

--- snippets/r.lua
+++ snippets/r.lua
@@ -1,49 +1,99 @@
 local ls = require("luasnip")
 local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
 local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 
-ls.add_snippets("r", {
-
-  s(":([-\\w.+*$]+)(?:([<>=])([^/>;]+)?)?", {
-    f(function(args, snip)
-      local m = string.match(args[1][1], "^([-]?%d[%d.]*L?|[-]|[.]|[+]|[*]|[$])([%a_].*)")
-      local augument, params
-
-      if m then
-        augument = m[2]
-        if m[1] == "+" then
-          params = "TRUE"
-        elseif m[1] == "-" then
-          params = "FALSE"
-        elseif m[1] == "." then
-          params = "NA"
-        elseif m[1] == "*" then
-          params = "NULL"
-        elseif m[1] == "$" then
-          params = augument
-        elseif m[1] == "00" then
-          params = "NULL"
-        else
-          params = m[1]
-        end
-      else
-        augument = args[1][1]
-        if not args[2][1] then
-          params = "TRUE"
-        elseif args[2][1] == ">" then
-          params = args[3][1] and args[3][1] .. ";" or "list;"
-        elseif args[2][1] == "=" then
-          local split = vim.split(args[3][1], "%s+")
-          params = #split > 1 and "c(" .. table.concat(split, ", ") .. ")" or split[1]
-        elseif args[2][1] == "<" then
-          local split = vim.split(args[3][1], "%s+")
-          local quoted = vim.tbl_map(function(s)
-            return '"' .. s .. '"'
-          end, split)
-          params = #quoted > 1 and "c(" .. table.concat(quoted, ", ") .. ")" or quoted[1]
-        end
-      end
-      return augument .. " = " .. params
-    end),
+return {
+  -- Rscript Header (Simple text snippet)
+  s({ trig = "libs", name = "Rscript Header", dscr = "Standard Rscript library imports" }, {
+    t({
+      "box::use(magrittr[`%>%`, `%<>%`, `%T>%`])",
+      "box::use(stringr[glue = str_glue])",
+      "box::use(data.table[setDT, as.data.table, data.table, setnames])",
+      "",
+    }),
+    i(0),
   }),
-})
+
+  -- local block (Word trigger - no change needed)
+  s({ trig = "local", name = "Local Block", dscr = "R local({...}) block", wordTrig = true }, {
+    t({ "local({", "\t" }),
+    i(1),
+    t({ "", "})" }),
+    i(0),
+  }),
+
+  -- showtext font setting (Simple text snippet - no change needed)
+  s({ trig = "showtext", name = "Showtext Setup", dscr = "Setup showtext library and font" }, {
+    t({
+      'library("showtext")',
+      "showtext_auto()",
+      'font_add("Noto Serif CJK SC", "NotoSerifCJK-Regular.ttc")',
+      "",
+    }),
+    i(0),
+  }),
+
+  -- vim modeline (Simple text snippet - no change needed)
+  s({ trig = "# vim", name = "Vim Modeline", dscr = "Add vim modeline" }, {
+    t("# vim: set tw=0 nowrap fdm=marker"),
+    i(0),
+  }),
+
+  -- Externally callable function with Roxygen comments (Regex trigger with ECMA engine using [[...]])
+  s({
+    trig = [[^@(\w+(?:,\w+)*)]], -- Much clearer!
+    regTrig = true,
+    trigEngine = "ecma",
+    name = "Exported Function Stub",
+    dscr = "Create exported function with Roxygen comments",
+  }, {
+    t("# "),
+    f(function(_, snip)
+      local args = snip.captures
+      local keywords_str = args[1]
+      -- Use Lua pattern match
+      local funname = string.match(keywords_str, "^[^,]+")
+      return funname or ""
+    end, { 1 }),
+    t({ "", "#' " }),
+    i(1, "Description"),
+    t({ "", "#'" }),
+    f(function(_, snip)
+      local args = snip.captures
+      local keywords_str = args[1]
+      local params_doc = { "" }
+      local keywords = vim.split(keywords_str, ",")
+      for idx = 2, #keywords do
+        table.insert(params_doc, "#' @param " .. keywords[idx])
+      end
+      t(params_doc)
+    end, { 1 }),
+    t({ "#' @return " }),
+    i(2, "NULL"),
+    t({ "", "#'", "#' @example", "#' NULL", "#' @export", "" }),
+    f(function(_, snip)
+      local keywords_str = snip.captures[1]
+      local keywords = vim.split(keywords_str, ",") -- Helper uses Lua patterns
+      local funname = keywords[1] or ""
+      local params_list = {}
+      for idx = 2, #keywords do
+        table.insert(params_list, keywords[idx])
+      end
+      local args_str = table.concat(params_list, ", ")
+      return funname .. " <- function(" .. args_str .. ")"
+    end, { 1 }),
+    t({ "", "{" }),
+    t({ "\t" }),
+    i(0),
+    t({ "", "}" }),
+  }),
+}
