690b99ee7a4b243b969b873bc91d78ce197bb74c
liu.bian.shi <liu.bian.shi@gmail.com>
2025-07-31 00:50:59 +0800

    fix(core.lua): correct background mode switching logic
    The previous logic was not correct, because it was not switching to dark mode between 00:00 and 06:00.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,11 +4,12 @@ return {
     opts = function(_, opts)
       local background
       local env_bg = vim.env.NVIM_BACKGROUND and vim.env.NVIM_BACKGROUND:lower()
+      local current_time = tonumber(os.date("%H"))
 
       if env_bg == "dark" or env_bg == "light" then
         background = env_bg
       else
-        background = tonumber(os.date("%H")) >= 18 and "dark" or "light"
+        background = (current_time >= 18 or current_time <= 6) and "dark" or "light"
       end
 
       vim.opt.background = background
